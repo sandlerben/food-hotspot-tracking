d7f907b50245e7b37702377c505176fd6328acec
Yoni Nachmany <nachmany@seas.upenn.edu>
2014-10-18 10:51:40 -0400

    Added models.py with json represented as db.Blob()

---
+++ models.py
@@ -0,0 +1,8 @@
+#from app import db
+import db
+
+class Tweet(db.Model):
+    tweets_dict = db.Column(db.Blob(), primary_key=True)
+
+    def __repr__(self):
+        return '<Tweet %r>' % (self.tweets_dict)
