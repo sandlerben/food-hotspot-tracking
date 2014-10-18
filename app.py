b6950f6b6efb21b4ce8e3224b0a425456909e188
Ben Sandler <ben1sandler@gmail.com>
2014-10-17 23:00:29 -0400

    added tokengetter

--- app.py
+++ app.py
@@ -1,30 +1,41 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify,session
 from flask_oauth import OAuth
 
 app = Flask(__name__)
 
 @app.route('/')
 def page():
-    #return render_template('base.html')
-    return get_tweets()
+	#return render_template('base.html')
+	return get_tweets()
 
 def get_tweets():
 	oauth = OAuth()
-    twitter = oauth.remote_app('twitter',base_url='https://api.twitter.com/1.1/',request_token_url='https://api.twitter.com/oauth/request_token',access_token_url='https://api.twitter.com/oauth/access_token',authorize_url='https://api.twitter.com/oauth/authenticate',consumer_key='YvNbcy4KN9tzndKL0GvVAQMGA',consumer_secret='NNsRAUo9D7RgtDbg2ksZIwfJfUszOHXPiBoLLC87TTu61p0Ejx')
-    resp = twitter.get('search/tweets.json', data = {
-        q: 'hungry',
-        geocode: '1,38,500km'
-        })
+	twitter = oauth.remote_app('twitter',base_url='https://api.twitter.com/1.1/',request_token_url='https://api.twitter.com/oauth/request_token',access_token_url='https://api.twitter.com/oauth/access_token',authorize_url='https://api.twitter.com/oauth/authenticate',consumer_key='YvNbcy4KN9tzndKL0GvVAQMGA',consumer_secret='NNsRAUo9D7RgtDbg2ksZIwfJfUszOHXPiBoLLC87TTu61p0Ejx')
 
-    if resp.status == 200:
-        tweets = resp.data
-    else:
-        tweets = None
-        flash('whoops - couldn\'t get tweets :(')
+	session['twitter_token'] = (
+        resp['oauth_token'],
+        resp['oauth_token_secret']
+    	)
 
-    print resp.status
+	@twitter.tokengetter
+	def get_twitter_token(token=None):
+	    return session.get('twitter_token')
 
-    return tweets
+	resp = twitter.get('search/tweets.json', data = {
+		'q': 'hungry',
+		'geocode': '1,38,500km'
+		})
+
+	if resp.status == 200:
+		tweets = resp.data
+	else:
+		tweets = None
+		flash('whoops - couldn\'t get tweets :(')
+
+
+	return tweets
+
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9393, debug=True)
+	app.run(host='0.0.0.0', port=9393, debug=True)
