9b33492fe5454a7f2936144570826c7513798e33
Yoni Nachmany <nachmany@seas.upenn.edu>
2014-10-18 10:59:59 -0400

    enabled CSRF in config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
-#CSRF_ENABLED = True
+CSRF_ENABLED = True
