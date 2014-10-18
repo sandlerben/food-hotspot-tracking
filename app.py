bad5aaa34709e22d0adc3d55ab0a6d4cc1b9cb20
Ben Sandler <ben1sandler@gmail.com>
2014-10-17 22:43:11 -0400

    fixed stupid thing

--- app.py
+++ app.py
@@ -3,36 +3,28 @@ from flask_oauth import OAuth
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9393, debug=True)
-
 @app.route('/')
 def page():
-    return render_template('base.html')
-    # return get_tweets()
+    #return render_template('base.html')
+    return get_tweets()
 
-# def get_tweets():
-#     twitter = oauth.remote_app(
-#         'twitter', 
-#         base_url='https://api.twitter.com/1.1/',
-#         request_token_url='https://api.twitter.com/oauth/request_token',
-#         access_token_url='https://api.twitter.com/oauth/access_token',
-#         authorize_url='https://api.twitter.com/oauth/authenticate',
-#         consumer_key='YvNbcy4KN9tzndKL0GvVAQMGA',
-#         consumer_secret='NNsRAUo9D7RgtDbg2ksZIwfJfUszOHXPiBoLLC87TTu61p0Ejx'
-#     )
+def get_tweets():
+	oauth = OAuth()
+    twitter = oauth.remote_app('twitter',base_url='https://api.twitter.com/1.1/',request_token_url='https://api.twitter.com/oauth/request_token',access_token_url='https://api.twitter.com/oauth/access_token',authorize_url='https://api.twitter.com/oauth/authenticate',consumer_key='YvNbcy4KN9tzndKL0GvVAQMGA',consumer_secret='NNsRAUo9D7RgtDbg2ksZIwfJfUszOHXPiBoLLC87TTu61p0Ejx')
+    resp = twitter.get('search/tweets.json', data = {
+        q: 'hungry',
+        geocode: '1,38,500km'
+        })
 
-#     resp = twitter.get('search/tweets.json', data = {
-#         q: 'hungry',
-#         geocode: '1,38,500km'
-#         })
+    if resp.status == 200:
+        tweets = resp.data
+    else:
+        tweets = None
+        flash('whoops - couldn\'t get tweets :(')
 
-#     if resp.status == 200:
-#         tweets = resp.data
-#     else:
-#         tweets = None
-#         flash('whoops - couldn\'t get tweets :(')
+    print resp.status
 
-#     print resp.status
+    return tweets
 
-#     # return tweets
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9393, debug=True)
