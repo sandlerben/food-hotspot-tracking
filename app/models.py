74c626a7e95c40dad5646e829e584e85c1fb75b7
Yoni Nachmany <nachmany@seas.upenn.edu>
2014-10-18 11:37:21 -0400

    Added html and coord fields to models

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from app import db
 
-class Tweet(db.Model):
-    tweets_dict = db.Column(db.Blob(), primary_key=True)
+class AllTweets(db.Model):
+    tweets_dict = db.Column(db.Blob())
+    tweets_html = db.Column(db.Blob())
+    tweets_coords = db.Column(db.Blob())
 
     def __repr__(self):
         return '<Tweet %r>' % (self.tweets_dict)
