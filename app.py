8ef3f02ece12b6c1cfe4e65c6c680ef998d44d36
Ben Sandler <ben1sandler@gmail.com>
2014-10-18 01:30:27 -0400

    not global

--- app.py
+++ app.py
@@ -5,25 +5,24 @@ app = Flask(__name__)
 
 app.secret_key = 't\xea\x85B\xda&\xc3\xdf\x9c\x8f=\xf7\xfa\xa0\xe6\xd3\xf7\x899\xdf\xc0\xdb\x7f<'
 
-oauth = OAuth()
-twitter = oauth.remote_app('twitter',base_url='https://api.twitter.com/1.1/',request_token_url='https://api.twitter.com/oauth/request_token',access_token_url='https://api.twitter.com/oauth/access_token',authorize_url='https://api.twitter.com/oauth/authenticate',consumer_key='MH1GPY8XpYgT9P5zVlWeDjHaQ',consumer_secret='cmq6yblCsQiXD9LVwKK7Xh5DcZTA3fwlNPykWMzVegDMOWMkAm')
-
-session['twitter_token'] = (
-	'2835780022-2UlRmdzQLVbel8qr2RiQsDzPnSOhUBqW8JYmIoE',
-	't7mHe7nZqPO1y9dz5tLQRQFcXJZlB4h5MUfi2pdPdO0pg'
-)
-
-@twitter.tokengetter
-def get_twitter_token(token=None):
-	return session.get('twitter_token')
-
-
 @app.route('/')
 def page():
 	return render_template('base.html')
 	# return jsonify(**get_tweets())
 
 def get_tweets():
+	oauth = OAuth()
+	twitter = oauth.remote_app('twitter',base_url='https://api.twitter.com/1.1/',request_token_url='https://api.twitter.com/oauth/request_token',access_token_url='https://api.twitter.com/oauth/access_token',authorize_url='https://api.twitter.com/oauth/authenticate',consumer_key='MH1GPY8XpYgT9P5zVlWeDjHaQ',consumer_secret='cmq6yblCsQiXD9LVwKK7Xh5DcZTA3fwlNPykWMzVegDMOWMkAm')
+
+	session['twitter_token'] = (
+		'2835780022-2UlRmdzQLVbel8qr2RiQsDzPnSOhUBqW8JYmIoE',
+		't7mHe7nZqPO1y9dz5tLQRQFcXJZlB4h5MUfi2pdPdO0pg'
+	)
+
+	@twitter.tokengetter
+	def get_twitter_token(token=None):
+		return session.get('twitter_token')
+
 	resp = twitter.get('search/tweets.json', data = {
 		'q': 'hungry',
 		'geocode': '1,38,500km'
@@ -38,6 +37,18 @@ def get_tweets():
 	return tweets
 
 def get_tweet_html(id):
+	oauth = OAuth()
+	twitter = oauth.remote_app('twitter',base_url='https://api.twitter.com/1.1/',request_token_url='https://api.twitter.com/oauth/request_token',access_token_url='https://api.twitter.com/oauth/access_token',authorize_url='https://api.twitter.com/oauth/authenticate',consumer_key='MH1GPY8XpYgT9P5zVlWeDjHaQ',consumer_secret='cmq6yblCsQiXD9LVwKK7Xh5DcZTA3fwlNPykWMzVegDMOWMkAm')
+
+	session['twitter_token'] = (
+		'2835780022-2UlRmdzQLVbel8qr2RiQsDzPnSOhUBqW8JYmIoE',
+		't7mHe7nZqPO1y9dz5tLQRQFcXJZlB4h5MUfi2pdPdO0pg'
+	)
+
+	@twitter.tokengetter
+	def get_twitter_token(token=None):
+		return session.get('twitter_token')
+
 	resp = twitter.get('statuses/oembed.json', data = {
 		'id': id
 		})
