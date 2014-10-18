dd5a023d309307492acfd4d9de90625641e60ba1
Ben Sandler <ben1sandler@gmail.com>
2014-10-18 02:14:48 -0400

    hungry tweets display on website

--- app.py
+++ app.py
@@ -7,8 +7,14 @@ app.secret_key = 't\xea\x85B\xda&\xc3\xdf\x9c\x8f=\xf7\xfa\xa0\xe6\xd3\xf7\x899\
 
 @app.route('/')
 def page():
-	return render_template('base.html')
-	# return jsonify(**get_tweets())
+	#return render_template('base.html')
+	#return jsonify(**get_tweets())
+	tweets_dict = get_tweets()
+	statuses = tweets_dict["statuses"]
+	html_list = []
+	for status in statuses:
+		html_list.append(get_tweet_html(status["id"]))
+	return render_template('base.html',html_list=html_list)
 
 def get_tweets():
 	oauth = OAuth()
