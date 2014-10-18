9beccb1a1012b53dba17190b6603a0f2c63d541e
Yoni Nachmany <yoninachmany@quad2-747.wireless-resnet.upenn.edu>
2014-10-18 01:15:51 -0400

    Added get_tweet_html function

--- app.py
+++ app.py
@@ -36,5 +36,31 @@ def get_tweets():
 
 	return tweets
 
+def get_tweet_html(id):
+	oauth = OAuth()
+	twitter = oauth.remote_app('twitter',base_url='https://api.twitter.com/1.1/',request_token_url='https://api.twitter.com/oauth/request_token',access_token_url='https://api.twitter.com/oauth/access_token',authorize_url='https://api.twitter.com/oauth/authenticate',consumer_key='MH1GPY8XpYgT9P5zVlWeDjHaQ',consumer_secret='cmq6yblCsQiXD9LVwKK7Xh5DcZTA3fwlNPykWMzVegDMOWMkAm')
+
+	session['twitter_token'] = (
+		'2835780022-2UlRmdzQLVbel8qr2RiQsDzPnSOhUBqW8JYmIoE',
+		't7mHe7nZqPO1y9dz5tLQRQFcXJZlB4h5MUfi2pdPdO0pg'
+	)
+
+	@twitter.tokengetter
+	def get_twitter_token(token=None):
+		return session.get('twitter_token')
+
+	resp = twitter.get('statuses/oembed.json', data = {
+		'id': id
+		})
+
+	if resp.status == 200:
+		tweet_html = resp.data
+	else:
+		tweet_html = None
+		flash('whoops - couldn\'t get tweets :(')
+
+	return tweet_html['html']
+
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=9393, debug=True)
