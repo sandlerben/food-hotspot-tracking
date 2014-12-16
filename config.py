aa57ec93bb7011fbe5f782dffb78343bd660353d
Ben Sandler <ben1sandler@gmail.com>
2014-12-15 21:36:04 -0500

    heroku prototype done

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',     
         'sqlite:///' + os.path.join(basedir, 'app.db'))
-SECRET_KEY =              os.getenv('SECRET_KEY',       '\xc4\xc2\xcfB0\xd7A\xf4A\xe8\xf4\xd5|x\xc1Q\xb32\x05\xde\x13\x10\xe1h')
+SECRET_KEY =              os.getenv('SECRET_KEY',       'not a real key')
 
 
 CSRF_ENABLED = True
